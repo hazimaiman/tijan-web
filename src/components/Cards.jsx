e0394d418bc2e631ea548d482e00e9df51b58abc
hazimaiman <aish22230105@gmail.com>
2023-10-15 11:21:12 +0800

    edit asset and cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import Single from '../assets/single.png'
-import Double from '../assets/double.png'
-import Triple from '../assets/triple.png'
+import Haji from '../assets/haji.png'
+import Azman from '../assets/azman.png'
+import Awang from '../assets/awang.png'
 
 const Cards = () => {
   return (
     <div className='w-full py-[10rem] px-4 bg-white'>
       <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8'>
           <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
-              <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Single} alt="/" />
+              <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Haji} alt="/" />
               <h2 className='text-2xl font-bold text-center py-8'>Dato Haji Hadi</h2>
               <div className='text-center font-medium'>
                   <p className='py-2 border-b mx-8 mt-8'>CEO</p>
@@ -18,7 +18,7 @@ const Cards = () => {
               <button className='bg-[#1c9bf0] text-white w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Connect</button>
           </div>
           <div className='w-full shadow-xl bg-gray-100 flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300'>
-              <img className='w-20 mx-auto mt-[-3rem] bg-transparent' src={Double} alt="/" />
+              <img className='w-20 mx-auto mt-[-3rem] bg-transparent' src={Azman} alt="/" />
               <h2 className='text-2xl font-bold text-center py-8'>Azman Yusof</h2>
               <div className='text-center font-medium'>
                   <p className='py-2 border-b mx-8 mt-8'>CFO</p>
@@ -28,7 +28,7 @@ const Cards = () => {
               <button className='bg-black text-white w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Connect</button>
           </div>
           <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
-              <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Triple} alt="/" />
+              <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Awang} alt="/" />
               <h2 className='text-2xl font-bold text-center py-8'>Haji Awang</h2>
               <div className='text-center font-medium'>
                   <p className='py-2 border-b mx-8 mt-8'>COO</p>
